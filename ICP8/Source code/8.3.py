d2152ff6ecf66115a003fc3042d1fd6346150cb6
rasheedalhazmi <77854174+rasheedalhazmi@users.noreply.github.com>
2021-03-12 22:39:36 -0600

    Add files via upload

---
+++ ICP8/Source code/8.3.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from keras.layers.core import Dense
+from keras.models import Sequential
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+dataset = pd.read_csv("breastcancer.csv")
+mapping = {'B': 1, 'M': 0}
+dataset = dataset.replace({'diagnosis': mapping})
+dataset = dataset.drop(columns=['id'])
+print(dataset.head())
+dataset = dataset.values
+
+scaler = StandardScaler()
+scaled_dataset = scaler.fit_transform(dataset[:, 0:31])
+
+X_train, X_test, Y_train, Y_test = train_test_split(scaled_dataset, dataset[:, 0], test_size=0.25, random_state=87)
+np.random.seed(155)
+my_first_nn = Sequential()  # create model
+my_first_nn.add(Dense(20, input_dim=31, activation='relu'))  # hidden layer
+my_first_nn.add(Dense(1, activation='sigmoid'))  # output layer
+my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100, initial_epoch=0)
+print("-" * 40)
+print(my_first_nn.summary())
+print("-" * 40)
+print(my_first_nn.evaluate(X_test, Y_test))
